49d6b158a8a988ee2730778decfab2f3e1ecda91
levabala <levabalaanother@gmail.com>
2022-10-09 16:42:25 +0400

    feat: add auto-linking script

---
+++ link_to_home.sh
@@ -0,0 +1,11 @@
+createLink() {
+  RELATIVE_PATH=$(realpath --relative-to=$HOME/.dotfiles $0)
+  echo $RELATIVE_PATH
+  mkdir -p $HOME/$(dirname $RELATIVE_PATH)
+  mkdir -p $HOME/.dotfiles_backup/$(dirname $RELATIVE_PATH)
+  mv $HOME/$RELATIVE_PATH $HOME/.dotfiles_backup/$RELATIVE_PATH
+  ln -s $0 $HOME/$RELATIVE_PATH
+}
+export -f createLink
+
+find $HOME/.dotfiles/ -type f ! -path "$HOME/.dotfiles/.git/*" ! -path "$HOME/.dotfiles/**/*.backup" -exec sh -c 'createLink "$0"' {} \;
