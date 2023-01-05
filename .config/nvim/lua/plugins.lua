d61084b345e8ae9f81d9aff199124bf437250138
levabala <levabala@ya.ru>
2023-01-05 10:32:16 +0100

    feat(vim): add fidget for lsp loading status

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -55,6 +55,7 @@ return require('packer').startup(function(use)
   use 'hrsh7th/cmp-path'
   use 'hrsh7th/cmp-cmdline'
   use 'hrsh7th/nvim-cmp'
+  use 'j-hui/fidget.nvim'
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
