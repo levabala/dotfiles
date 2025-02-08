94b7048b93ef6a5eede20854337217ff8519cc74
levabala <levabalaanother@gmail.com>
2025-02-08 09:54:45 +0100

    fix: load arcadia plugins only when one's mounted

--- .config/nvim/lua/plugins/arcadia.lua
+++ .config/nvim/lua/plugins/arcadia.lua
@@ -1,4 +1,9 @@
+local function exists(path)
+	return vim.loop.fs_stat(path) ~= nil
+end
+
 return {
-	{ dir = "~/arcadia/devtools/vim/plugin_bundles/signify" },
-	{ dir = "~/arcadia/devtools/vim/plugin_bundles/vcscommand" },
+	{ dir = "~/arcadia/devtools/vim/plugin_bundles/signify", cond = exists(vim.fn.expand("~/arcadia")) },
+	{ dir = "~/arcadia/devtools/vim/plugin_bundles/vcscommand", cond = exists(vim.fn.expand("~/arcadia")) },
+	enabled = false,
 }
